c5b39109fed4304f0265c60421287192bba52526
Bismullah Wafadar <bismullahwafadar5@gmail.com>
2025-06-28 21:46:24 +0430

    added changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 import Hero from "@/components/Hero";
+import AboutMe from "@/components/AboutMe";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
+    <>
+    <Navbar />
     <main>
       <Hero name="Bismillah" title="Frontend Developer passionate about building fast and modern web apps." />
-      {/* You can add <Projects /> section with id="projects" below */}
+      <AboutMe />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
     </main>
+    </>
   );
 }
